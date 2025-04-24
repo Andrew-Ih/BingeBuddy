471a6fc0e2042fcf4d6c821cc46fc890f0730f6e
Andrew.I <andrewiheanacho811@gmail.com>
2025-04-24 14:44:30 -0500

    Added language and release date (year) to movie card, completed movie card component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MovieCard = ({movie: {title, vote_average, poster_path, original_language }}) => {
+const MovieCard = ({movie: {title, vote_average, poster_path, original_language, release_date }}) => {
   return (
     <div className="movie-card">
         <img src={poster_path ? 
@@ -16,6 +16,12 @@ const MovieCard = ({movie: {title, vote_average, poster_path, original_language 
                     <img src="star.svg" alt="Star Icon"/>
                     <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                 </div>
+
+                <span>•</span>
+                <p className='lang'>{original_language}</p>
+                <span>•</span>
+                <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+
             </div>
         </div>
     </div>
